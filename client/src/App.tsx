43a4e1b5e539e3ec3316c8b2c34e65d70fbeee33
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-20 04:54:56 +0800

    landing page initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Landing from './pages/Landing/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <div>
       <h1>Jobify</h1>
+      <Landing />
     </div>
   )
 }
