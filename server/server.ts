e805a9d64b905e07ad69088548b96e6a9cab3ff3
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-19 09:47:24 +0800

    dotenv port integration

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,8 @@ const app = express()
 
 dotenv.config()
 
-const PORT = 3000
+// const PORT = 3000
+const PORT = process.env.PORT
 console.log(`server running on port : ${PORT}`)
 
 // app.listen(3000)
