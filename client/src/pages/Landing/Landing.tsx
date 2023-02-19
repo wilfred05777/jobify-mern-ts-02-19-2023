6f1ad31e67fa4762c385ffef0286e20e75f9d0ac
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-20 05:18:03 +0800

    14. Landing Page - Structure

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import logo from '../../assets/images/logo.svg'
+import main from '../../assets/images/main.svg'
+import Link from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <div>
-      <h2>Landing page</h2>
-    </div>
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>Tracking</span> app
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          {/* <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link> */}
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
   )
 }
 
