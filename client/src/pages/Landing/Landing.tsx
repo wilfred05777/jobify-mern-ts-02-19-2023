1faf4954e55dfdcbe4aec7380dd023731e5884a2
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-20 05:36:50 +0800

    good thing to know about styled-components

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -4,11 +4,22 @@ import main from '../../assets/images/main.svg'
 import Link from 'react-router-dom'
 import styled from 'styled-components'
 
-const Button = styled.button``
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`
+const ButtonSecond = styled.button`
+  background: blue;
+  color: white;
+  font-size: 1rem;
+`
 
 const Landing = () => {
   return (
     <main>
+      <Button>Click Me</Button>
+      <ButtonSecond>Click Me</ButtonSecond>
       <nav>
         <img src={logo} alt='jobify' className='logo' />
       </nav>
