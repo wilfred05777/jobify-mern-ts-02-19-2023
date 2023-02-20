bfe283d08438c571bd9ce037caa18b9106954d49
Wilfred Erdo <wilfred05777@gmail.com>
2023-02-20 09:25:17 +0800

    16. Landing Page - Complete

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import logo from '../../assets/images/logo.svg'
 import main from '../../assets/images/main.svg'
 import Link from 'react-router-dom'
+import './landing.css'
+
 import styled from 'styled-components'
 
 const Landing = () => {
@@ -33,12 +35,40 @@ const Landing = () => {
 }
 
 const Wrapper = styled.main`
-  width: var(--fluid-width);
-  max-width: var(--max-width);
-  margin: 0 auto;
-  height: var(--nav-height);
-  display: flex;
-  align-items: center;
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+    }
+    .main-img {
+      display: block;
+    }
+  }
 `
 
 export default Landing
